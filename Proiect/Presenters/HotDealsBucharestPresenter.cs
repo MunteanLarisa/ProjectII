d7e1c1ff75221e5bcb3979c95daccb9670256b0a
bara-iulia-30127 <99618894+bara-iulia-30127@users.noreply.github.com>
2023-05-20 23:23:28 +0300

    update

--- Proiect/Presenters/HotDealsBucharestPresenter.cs
+++ Proiect/Presenters/HotDealsBucharestPresenter.cs
@@ -22,7 +22,7 @@ namespace Proiect.Presenters
         {
             SqlConnection myCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Proiect_II\ProjectII\Proiect\Database.mdf;Integrated Security=True");
             myCon.Open();
-            SqlDataAdapter da = new SqlDataAdapter("SELECT SUGGESTIONS, ACCOMODATION_TYPE, REVIEW, CENTER_DISTANCE, MEALS_INCLUDED, NO_NIGHTS, (SUGESTII.BUDGET+CAZARE.BUDGET) AS TOTAL FROM SUGESTII FULL OUTER JOIN CAZARE ON SUGESTII.ID=CAZARE.ID WHERE SUGESTII.ID=3 AND (SUGESTII.BUDGET+CAZARE.BUDGET) BETWEEN 80 AND 159", myCon);
+            SqlDataAdapter da = new SqlDataAdapter("SELECT SUGGESTIONS, ACCOMODATION_TYPE AS 'ACCOMODATION', REVIEW AS 'REVIEW OUT OF 5', CENTER_DISTANCE AS 'DISTANCE FROM CENTER(m)', MEALS_INCLUDED AS 'MEALS INCLUDED', NO_NIGHTS AS 'NIGHTS', (SUGESTII.BUDGET+CAZARE.BUDGET) AS 'TOTAL(EURO)' FROM SUGESTII FULL OUTER JOIN CAZARE ON SUGESTII.ID=CAZARE.ID WHERE SUGESTII.ID=3 AND (SUGESTII.BUDGET+CAZARE.BUDGET) BETWEEN 80 AND 159", myCon);
             DataTable dt = new DataTable();
             da.Fill(dt);
             hotDealsBucharest.gridView = dt;
